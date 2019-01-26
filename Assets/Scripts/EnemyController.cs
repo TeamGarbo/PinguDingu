ea674c759edacccb4a408af89cf1d8a8b4bfe67c
Jack Trute <jack@trute.co.uk>
2019-01-26 12:28:43 +0000

    Started enemy controller script.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using static GameController;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player;
+    public int moveSpeed = 5;
+    public int maxDist = 10;
+    public int minDist = 0;
+
+
+    void Awake ()
+    {
+        player = GameController.GetPlayer().transform;
+    }
+
+
+    void FixedUpdate ()
+    {
+        transform.LookAt(player);
+
+        // If the enemy is away from the player
+        if (Vector3.Distance(transform.position, player.position) >= minDist) {
+            // Move toward the player
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed);
+        } else {
+            // die
+        }
+    } 
+}
