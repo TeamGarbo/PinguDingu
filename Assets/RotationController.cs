59e297c448d2e66e3fb12449ac18564fa85df298
Mikolaj <mfb2g15@soton.ac.uk>
2019-01-26 11:33:51 +0000

    Started making the main menu screen

---
+++ Assets/RotationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+	public float rotationSpeed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
