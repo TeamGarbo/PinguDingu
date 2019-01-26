e2296ea2a9ef5c52d000a6e93643d31a5e635837
Tomas Piga <tomp@theodo.co.uk>
2019-01-26 18:08:35 +0000

    [12] Store item in igloo when inside

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour {
+    // private static Vector3[] locations = [
+    //     new Vector3(0, 0, -1),
+    //     new Vector3(-1, 0, -1)
+    // ];
+
+    private static int numberOfItemsInHouse = 0;
 
     public void DoTheThing() {
         transform.SetParent(GameController.GetPlayer().transform);
         transform.localPosition = new Vector3(0, 1, -1);
+    }
+
+    public void PlaceInsideHouse() {
+        transform.SetParent(GameController.GetIgloo().transform);
+        transform.localPosition = new Vector3(1.469f, 0.3104561f, 2.492f);
+        transform.localPosition = new Vector3(0.827f, 0.09f, 0.125f);
     }
 }
