7d79bad1ab300336fc65cd9aaf52f95b40c9c0b4
Chrriiisss <christopher.smith6@hotmail.co.uk>
2019-01-26 10:55:29 +0000

    Added Moving up/down code
    Water movement done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float velocityLimit;
+    public float velocity;
+
+    // Start is called before the first frame update
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (Input.GetKey(KeyCode.Space)) {
+            rb.AddForce(Vector3.up * jumpPower);
+        }
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            rb.AddForce(Vector3.down * jumpPower);
+        }
+        velocity = rb.velocity.y;
+        if (rb.velocity.y > velocityLimit) {
+            rb.velocity = new Vector3(rb.velocity.x, velocityLimit, rb.velocity.z);
+        }
+        else if (rb.velocity.y < -velocityLimit) {
+            rb.velocity = new Vector3(rb.velocity.x, -velocityLimit, rb.velocity.z);
+        }
+    }
+}
