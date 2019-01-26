c3e42ff3bd178c58c9111d38f7e43da6d50cedf2
Mikolaj <mfb2g15@soton.ac.uk>
2019-01-26 18:03:02 +0000

    Main menu looks done

---
+++ Assets/Scripts/TextBopController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBopController : MonoBehaviour
+{
+
+    Vector2 floatY;
+    float originalY;
+    public float bobStrength;
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        floatY = transform.position;
+        floatY.y = originalY + (Mathf.Sin(Time.time + offset) * bobStrength);
+        transform.position = floatY;
+    }
+}
