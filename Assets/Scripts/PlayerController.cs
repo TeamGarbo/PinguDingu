33a6bb06a8b48fb9edbd5350829f56a703618096
Chrriiisss <christopher.smith6@hotmail.co.uk>
2019-01-26 12:35:02 +0000

    Added Underwater Fog

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private readonly float jumpPower = 100;
     private readonly float velocityLimit = 4;
-    [SerializeField] private readonly float waterOffset = -1.1f;
+    [SerializeField] private readonly float waterOffset = -1.6f;
     private float belowWaterAmount;
     public float velocity;
 
@@ -20,7 +20,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() {
         if (transform.position.y < belowWaterAmount) {
-            // ----------------------------------MOVEMENT CODE----------------------------------
+            if (!RenderSettings.fog) {
+                RenderSettings.fog = true;
+            }
+            // ----------------------------------WATER CODE----------------------------------
             if (Input.GetKey(KeyCode.Space)) {
                 rb.AddForce(Vector3.up * jumpPower);
             }
@@ -35,6 +38,9 @@ public class PlayerController : MonoBehaviour
                 rb.velocity = new Vector3(rb.velocity.x, -velocityLimit, rb.velocity.z);
             }
             // ------------------------------------------------------------------------------
+        }
+        else if (RenderSettings.fog) {
+            RenderSettings.fog = false;
         }
 
         if (Input.GetKey(KeyCode.E)) {
