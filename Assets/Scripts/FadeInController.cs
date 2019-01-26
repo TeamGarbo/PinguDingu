2b14d55e688979dbb07629551741895309e59693
Mikolaj <mfb2g15@soton.ac.uk>
2019-01-26 21:05:34 +0000

    hmmmmm

---
+++ Assets/Scripts/FadeInController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInController : MonoBehaviour
+{
+    public float fadeSpeed = 1;
+    public Image image;
+
+    void Awake() {
+        image = GetComponent<Image>();
+        var tempColor = image.color;
+        tempColor.a = 0f;
+        image.color = tempColor;
+    }
+    void Update() {
+        var tempColor = image.color;
+        tempColor.a += Time.deltaTime * fadeSpeed;
+        image.color = tempColor;
+    }
+    
+}
