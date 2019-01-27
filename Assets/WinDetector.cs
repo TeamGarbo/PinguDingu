2651ca6b9a539352754f5e7256e6c4526f0d5e6c
Samuel Moorcroft <samuel.moorcroft@premfina.com>
2019-01-27 13:21:42 +0000

    added script to detect end of game

---
+++ Assets/WinDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] allItems = GameObject.FindGameObjectsWithTag("Item");
+        int numCollected = 0;
+        foreach (GameObject item in allItems) {
+            if(item.transform.parent == GameController.GetIgloo().transform){
+                numCollected++;
+            }
+        }
+        if (numCollected == allItems.Length){
+            gameWon();
+        }
+        Debug.Log(numCollected);
+    }
+
+    private void gameWon(){
+        SceneManager.LoadScene("VictoryScene");
+    }
+}
