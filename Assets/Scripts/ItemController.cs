423b3e99aaff2d16b5168609b36faf661d827a17
Chrriiisss <christopher.smith6@hotmail.co.uk>
2019-01-27 11:07:26 +0000

    Dear lord the changes

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour {
-    // private static Vector3[] locations = [
-    //     new Vector3(0, 0, -1),
-    //     new Vector3(-1, 0, -1)
-    // ];
 
     private static int numberOfItemsInHouse = 0;
 
+    [SerializeField] private Vector3 carryPosition;
+    [SerializeField] private Vector3 carryRotation;
+    private Vector3 originalRotation;
+
     public void DoTheThing() {
         transform.SetParent(GameController.GetPlayer().transform.GetChild(1));
-        transform.localPosition = new Vector3(0, 2, -1);
+        originalRotation = transform.eulerAngles;
+        transform.localPosition = carryPosition;
+        transform.eulerAngles = carryRotation;
+        if (GetComponent<Collider>() != null) {
+            GetComponent<Collider>().enabled = false;
+        }
     }
 
     public void PlaceInsideHouse() {
@@ -23,5 +28,7 @@ public class ItemController : MonoBehaviour {
 
     public void Drop() {
         transform.SetParent(GameController.GetIgloo().transform);
+        transform.eulerAngles = originalRotation;
+        transform.position = new Vector3(transform.position.x, GameController.GetPlayer().transform.position.y+0.5f, transform.position.z);
     }
 }
